ad4bbcd2a38def44231a4ed9cbca863e0e0d68dd
SergioCayetano <sergiocayetano18@gmail.com>
2026-07-07 18:56:54 +0200

    Pantalla de test: preguntas, opciones y navegacion

--- src/app/pages/test/test.ts
+++ src/app/pages/test/test.ts
@@ -1,15 +1,55 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Pregunta } from '../../models/pregunta';
+import { PreguntasService } from '../../services/preguntas.service';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './test.html',
   styleUrl: './test.css',
 })
 export class Test {
   private ruta = inject(ActivatedRoute);
+  private preguntasService = inject(PreguntasService);
 
-  // el parámetro :tema de la URL
   tema = this.ruta.snapshot.paramMap.get('tema') ?? '';
+
+  // hasta 25 preguntas barajadas del tema
+  preguntas: Pregunta[] = this.preguntasService.getAleatorias(this.tema, 25);
+
+  indice = 0;
+
+  // opción elegida por pregunta, null = en blanco
+  respuestas: (number | null)[] = this.preguntas.map(() => null);
+
+  get preguntaActual(): Pregunta {
+    return this.preguntas[this.indice];
+  }
+
+  get esUltima(): boolean {
+    return this.indice === this.preguntas.length - 1;
+  }
+
+  get respondidas(): number {
+    return this.respuestas.filter((r) => r !== null).length;
+  }
+
+  seleccionar(opcion: number) {
+    // repetir clic en la misma opción la desmarca
+    this.respuestas[this.indice] =
+      this.respuestas[this.indice] === opcion ? null : opcion;
+  }
+
+  siguiente() {
+    if (!this.esUltima) this.indice++;
+  }
+
+  anterior() {
+    if (this.indice > 0) this.indice--;
+  }
+
+  letra(i: number): string {
+    return ['a', 'b', 'c'][i];
+  }
 }
