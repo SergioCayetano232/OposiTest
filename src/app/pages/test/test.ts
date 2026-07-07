a91426f76de8a406265fd2de6b969c6f18093036
SergioCayetano <sergiocayetano18@gmail.com>
2026-07-07 19:12:26 +0200

    Correccion con la puntuacion real del examen

--- src/app/pages/test/test.ts
+++ src/app/pages/test/test.ts
@@ -23,6 +23,8 @@ export class Test {
   // opción elegida por pregunta, null = en blanco
   respuestas: (number | null)[] = this.preguntas.map(() => null);
 
+  terminado = false;
+
   get preguntaActual(): Pregunta {
     return this.preguntas[this.indice];
   }
@@ -51,5 +53,42 @@ export class Test {
 
   letra(i: number): string {
     return ['a', 'b', 'c'][i];
+  }
+
+  corregir() {
+    this.terminado = true;
+  }
+
+  // nuevo intento con otras preguntas barajadas
+  repetir() {
+    this.preguntas = this.preguntasService.getAleatorias(this.tema, 25);
+    this.respuestas = this.preguntas.map(() => null);
+    this.indice = 0;
+    this.terminado = false;
+  }
+
+  get aciertos(): number {
+    return this.preguntas.filter((p, i) => this.respuestas[i] === p.correcta).length;
+  }
+
+  get fallos(): number {
+    return this.preguntas.filter(
+      (p, i) => this.respuestas[i] !== null && this.respuestas[i] !== p.correcta
+    ).length;
+  }
+
+  get enBlanco(): number {
+    return this.respuestas.filter((r) => r === null).length;
+  }
+
+  // como en el examen: acierto 1, fallo -1/3, blanco 0
+  get puntos(): number {
+    const brutos = this.aciertos - this.fallos / 3;
+    return Math.round(brutos * 100) / 100;
+  }
+
+  get nota(): number {
+    const sobre10 = (this.puntos / this.preguntas.length) * 10;
+    return Math.max(0, Math.round(sobre10 * 100) / 100);
   }
 }
